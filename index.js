17b532e728ce293b0f7bab70f8f02a5ab4eb1af6
congminh090800@gmail.com <congminh090800@gmail.com>
2022-03-06 20:54:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+require('envkey');
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res, next) => {
+    res.json(process.env.SECRET);
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT} 🚀`);
+})
