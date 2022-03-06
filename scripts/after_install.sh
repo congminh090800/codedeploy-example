b11376f8915d111c0fa7bc0b0a71f5e1992e6f76
congminh090800@gmail.com <congminh090800@gmail.com>
2022-03-07 00:12:51 +0700

    scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
+echo "-------------Move to working directory---------------"
 mkdir /home/ubuntu/codedeploy-example
 cd /home/ubuntu/codedeploy-example
+echo "Current directory $PWD"
+echo "-------------Moved---------------"
 # Update env vars
+echo "-------------Get env---------------"
+printenv ENVKEY
 curl -s https://raw.githubusercontent.com/envkey/envkey-source/master/install.sh | bash
 eval $(envkey-source)
+echo "-------------Got it---------------"
+echo "-------------Installing dependencies---------------"
 sudo npm install
+echo "-------------Installed dependencies---------------"
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "-------------Installing Node and PM2---------------"
 sudo apt update
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt -y install nodejs
 sudo apt -y install gcc g++ make
 sudo npm install pm2@latest -g
+echo "-------------Installed Node and PM2---------------"
